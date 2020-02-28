ec41525a8c3298898332dfae6c58de307dfb421c
jkpubsrc <pubsrc@binary-overflow.de>
2020-02-28 21:20:18 +0100

    Fixed another dependency error.

--- src/setup.py
+++ src/setup.py
@@ -24,7 +24,7 @@ setup(
 		"Topic :: System :: Monitoring",
 	],
 	description = "This python module provides ways to retrieve and parse technical data of (Linux) computer systems.",
-	download_url = "https://github.com/jkpubsrc/python-module-jk-sysinfo/tarball/0.2020.2.28",
+	download_url = "https://github.com/jkpubsrc/python-module-jk-sysinfo/tarball/0.2020.2.28.1",
 	include_package_data = False,
 	install_requires = [
 		"python-dateutil",
@@ -32,6 +32,7 @@ setup(
 		"pytz",
 		"jk_console",
 		"jk_typing",
+		"jk_version",
 	],
 	keywords = [
 		"monitoring",
@@ -45,7 +46,7 @@ setup(
 		"bin/sysinfo.py",
 	],
 	url = "https://github.com/jkpubsrc/python-module-jk-sysinfo",
-	version = "0.2020.2.28",
+	version = "0.2020.2.28.1",
 	zip_safe = False,
 	long_description = readme(),
 	long_description_content_type="text/markdown",
